8a4339b7bbfd91869721007c7758c4dc94c21b9d
Mrhalix <aminaleahmad@yahoo.com>
2016-04-15 19:12:46 +0430

    Create about.lua

---
+++ plugins/about.lua
@@ -0,0 +1,53 @@
+local command = 'about'
+local doc = '`Returns information about the bot.`'
+
+local triggers = {
+	''
+}
+
+local action = function(msg)
+
+	-- Filthy hack, but here is where we'll stop forwarded messages from hitting
+	-- other plugins.
+	if msg.forward_from then
+		if msg.chat.type == 'private' or msg.from.id == config.admin then
+		local fwdinfo = '#ID => '..msg.forward_from.id
+		..'\n#Name => '..msg.forward_from.first_name
+		..'\n#Username => @'..msg.forward_from.username
+		 ..'\n\nPinkPanther🐆'
+	senduserprofilephotos(msg.chat.id,msg.forward_from.id,fwdinfo)
+	--sendMessage(msg.chat.id,msg.forward_from.id)
+		return
+			end
+	end
+
+	local message = config.about_text
+
+	if msg.new_chat_participant and msg.new_chat_participant.id == bot.id then
+		sendMessage(msg.chat.id, message, true,nil,true)
+		return
+	elseif msg.text_lower:match('^/about[@'..bot.username..']*') then
+		sendMessage(msg.chat.id, message, true,nil,true)
+		return
+	elseif msg.text_lower:match('^/start') then
+		sendMessage(msg.chat.id, message, true,nil,true)
+		return
+	elseif msg.text_lower:match('^'..bot.name) then
+		sendMessage(msg.chat.id, 'Yes?', true,nil,true)
+		return
+	elseif msg.text_lower:match('^hi') then
+		sendMessage(msg.chat.id, 'Hello my name is *'..bot.first_name..'*\n send /help for me ☺️', true,nil,true)
+		return
+	end
+	
+
+	return true
+
+end
+
+return {
+	action = action,
+	triggers = triggers,
+	doc = doc,
+	command = command
+}
