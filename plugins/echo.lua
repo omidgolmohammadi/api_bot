a3d02b600ee770a684b58e808a7ab8666f6cba82
Mrhalix <aminaleahmad@yahoo.com>
2016-04-15 19:28:08 +0430

    Create echo.lua

---
+++ plugins/echo.lua
@@ -0,0 +1,35 @@
+local command = 'echo <text>'
+local doc = [[```
+/echo <text>
+Repeats a string of text.
+```]]
+
+local triggers = {
+	'^/echo[@'..bot.username..']*'
+}
+
+local action = function(msg)
+
+	local input = msg.text:input()
+
+	if not input then
+		sendMessage(msg.chat.id, doc, true, msg.message_id, true)
+	else
+		local output
+		if msg.chat.type == 'supergroup' then
+			output = 'Echo:\n"' .. input .. '"'
+		else
+			output = latcyr(input)
+		end
+		sendMessage(msg.chat.id, output, true,nil,true)
+	end
+
+
+end
+
+return {
+	action = action,
+	triggers = triggers,
+	doc = doc,
+	command = command
+}
