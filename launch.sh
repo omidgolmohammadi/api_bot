f4cd36430c29ddb03408d6533713f511c45ef3f2
Mrhalix <aminaleahmad@yahoo.com>
2016-04-16 16:37:52 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,18 +5,10 @@ update() {
   git pull
 	}
 
-install() {
-	sudo apt-get upgrade
-	sudo apt-get install lua5.2
-  	sudo apt-get install lua-socket
-	sudo apt-get install lua-sec
-	sudo apt-get install lua-cjson
-	sudo apt-get install curl
-}
 
-if [ "$1" = "install" ]; then
-  install
-elif [ "$1" = "update" ]; then
+
+
+if [ "$1" = "update" ]; then
   update
   fi
 		
